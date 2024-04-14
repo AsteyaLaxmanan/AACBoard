292d76fd41127c6db9c145f48160be77aec58008
AsteyaLaxmanan <68880458+AsteyaLaxmanan@users.noreply.github.com>
2024-04-13 20:01:27 -0700

    XCode Files pushed

---
+++ AACBoardApp/AACBoardApp/AACBoardAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  AACBoardAppApp.swift
+//  AACBoardApp
+//
+//  Created by Asteya Laxmanan on 4/13/24.
+//
+
+import SwiftUI
+
+@main
+struct AACBoardAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
