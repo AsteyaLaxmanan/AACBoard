294107f919576a727c90f19048e34ad69033890f
Shivanth Kandasamy <104394874+ShivanthK@users.noreply.github.com>
2024-04-13 23:11:28 -0700

    frontend - basic button implementation
    multiple pages that you can navigate through.

--- AACBoardApp/AACBoardApp/ContentView.swift
+++ AACBoardApp/AACBoardApp/ContentView.swift
@@ -1,26 +1,97 @@
 //
 //  ContentView.swift
-//  AACBoardApp
+//  AAS_Board
 //
-//  Created by Asteya Laxmanan on 4/13/24.
+//  Created by Shivanth Kandasamy on 3/23/24.
 //
 
 import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
-        }
-        .padding()
+        
+        Text("Title Filler")
+            .font(.title)
+        
+        Text("Sentence Builder Filler")
+            .frame(maxWidth: .infinity, minHeight: 50)
+            .background(Color.blue)
+        
+        Categories()
+        
     }
 }
 
-struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
+struct Categories: View {
+    
+    @State var tests = ["Asteya 0", "Neel 1", "Gaurwik 2", "Shiv 3", "Ani 4", "Ayaan 5", "Reya 6"]
+    let cols = 2 // must have AT LEAST 2 cols --> if not, code breaks
+    
+    var body: some View {
+        Text("Suggestions Filler")
+            .frame(maxWidth: .infinity, minHeight: 50)
+            .background(Color.green)
+
+        Catalog(tests: $tests, cols: cols)
     }
+}
+
+struct Catalog: View {
+    
+    @Environment(\.presentationMode) var mode
+    @Binding var tests: [String]
+    let cols: Int
+    
+    var body: some View {
+        
+        @State var rows = ((tests.indices.last ?? 0) + 1) / cols
+        @State var lastRow = ((tests.indices.last ?? 0) + 1) % cols
+        
+        ScrollView {
+            
+            HStack{
+                ForEach(0..<cols, id: \.self) { col in
+                    LazyVStack {
+                        ForEach(0..<rows, id: \.self) { row in
+                            ZStack {
+                                RoundedRectangle(cornerRadius: 25.0)
+                                    .fill(Color.red)
+                                    .frame(width: 150, height: 100)
+                                    .padding()
+                                Text(tests[row * 2 + col])
+                            }
+                        }
+                    
+                    }
+                }
+                
+            }
+            HStack{
+                ForEach(0..<lastRow, id: \.self) { index in
+                    ZStack {
+                        RoundedRectangle(cornerRadius: 25.0)
+                            .fill(Color.red)
+                            .frame(width: 150, height: 100)
+                            .padding()
+                        Text(tests[rows * cols + index])
+                    }
+                }
+            }
+            
+            Button {
+                tests = ["Asteya 0"]
+            } label: {
+                Text("Test Button")
+                    .padding()
+                    .background(Color.yellow)
+            }
+
+        }
+        
+    
+    }
+}
+
+#Preview {
+    ContentView()
 }
